58a3c1da478b63ddeecc7586db67e84a42e9d823
Akhil Gopal <agopal45@gatech.edu>
2025-04-08 11:29:38 -0400

    creates views and html pages to edit and delete transactions from transaction list

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.transaction_list, name='transaction_list'),
     path('add/', views.add_transaction, name='add_transaction'),
+    path('edit/<int:pk>/', views.edit_transaction, name='edit_transaction'),
+    path('delete/<int:pk>/', views.delete_transaction, name='delete_transaction'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Transaction
 from .forms import TransactionForm
+from django.shortcuts import get_object_or_404
 
 @login_required
 def add_transaction(request):
@@ -20,3 +21,23 @@ def add_transaction(request):
 def transaction_list(request):
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
     return render(request, 'transactions/transaction_list.html', {'transactions': transactions})
+
+@login_required
+def edit_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm(instance=transaction)
+    return render(request, 'transactions/edit_transaction.html', {'form': form})
+
+@login_required
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('transaction_list')
+    return render(request, 'transactions/delete_transaction.html', {'transaction': transaction})

6e3e7b4fd0f234db208eae8dfd9779cf97f9bb2f
Nicholes Cortes <187998850+ncortes9@users.noreply.github.com>
2025-04-21 11:43:15 -0400

    styling

--- agent/forms.py
+++ agent/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ChatForm(forms.Form):
     your_message = forms.CharField(label="", widget=forms.TextInput(attrs={
-            "class": "rounded-3 p-1 w-100 border",
-            "placeholder": "Type your message..."
+            "class": "rounded-pill   p-1 w-100 border",
+            "placeholder": "   Type your message..."
         }))

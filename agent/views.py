368bbbb6201f36f4717391ca445d00c77ed6443f
Nicholes Cortes <187998850+ncortes9@users.noreply.github.com>
2025-04-20 22:24:00 -0400

    agent remembers previous chats

--- agent/views.py
+++ agent/views.py
@@ -4,16 +4,15 @@ from google import genai
 from config import API_KEY
 
 # Create your views here.
+client = genai.Client(api_key=API_KEY)
+gem_chat = client.chats.create(model="gemini-2.0-flash")
 def chat(request):
-    client = genai.Client(api_key=API_KEY)
 
     if request.method == 'POST':
         form = ChatForm(request.POST)
         if form.is_valid():
             request.session['message_list'].append(form.cleaned_data['your_message'])
-            response = client.models.generate_content(
-                model="gemini-2.0-flash", contents=form.cleaned_data['your_message']
-            )
+            response = gem_chat.send_message(request.session['message_list'][-1])
             request.session['message_list'].append(response.text)
             request.session.modified = True
     else: # first time we visit the chat page

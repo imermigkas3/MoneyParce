2b54c0eaed4f7c228222f6ef83fd26754b45ebf2
Nicholes Cortes <187998850+ncortes9@users.noreply.github.com>
2025-04-20 22:10:03 -0400

    basic agent chatting

--- agent/views.py
+++ agent/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .forms import ChatForm
+from google import genai
+from config import API_KEY
 
 # Create your views here.
 def chat(request):
-    user = request.user
+    client = genai.Client(api_key=API_KEY)
 
     if request.method == 'POST':
         form = ChatForm(request.POST)
         if form.is_valid():
             request.session['message_list'].append(form.cleaned_data['your_message'])
+            response = client.models.generate_content(
+                model="gemini-2.0-flash", contents=form.cleaned_data['your_message']
+            )
+            request.session['message_list'].append(response.text)
             request.session.modified = True
     else: # first time we visit the chat page
         request.session['message_list'] = []

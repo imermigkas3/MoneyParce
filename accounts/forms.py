9d4a48b87766e353e2f59e85d3359ffa9a10a739
Akhil Gopal <agopal45@gatech.edu>
2025-04-23 22:29:29 -0400

    adds phone number and username

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,6 +8,7 @@ from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django import forms
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 class CustomErrorList(ErrorList):
     def __str__(self):
@@ -39,6 +40,12 @@ class CustomUserCreationForm(UserCreationForm):
             raise ValidationError("A user already with this email exists. Please use a different email address.")
         return email
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("A user with this username already exists. Please choose a different username.")
+        return username
+
 class CustomPasswordResetForm(PasswordResetForm):
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -59,16 +66,31 @@ class IncomeForm(forms.ModelForm):
         fields = ['amount']
 
 class UserProfileForm(forms.ModelForm):
+    username = forms.CharField(
+        required=True,
+        label='Username',
+        max_length=150,
+        validators=[UnicodeUsernameValidator()])
     email = forms.EmailField(required=True, label='Email Address')
 
     class Meta:
         model = UserProfile
-        fields = ['first_name', 'last_name', 'status']
+        fields = ['first_name', 'last_name', 'status', 'phone_number']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance and self.instance.user:
             self.fields['email'].initial = self.instance.user.email
+            self.fields['username'].initial = self.instance.user.username
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if self.instance and self.instance.user and self.instance.user.username == username:
+            return username  # No change, so it's valid
+
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username address is already in use.")
+        return username
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -81,8 +103,11 @@ class UserProfileForm(forms.ModelForm):
 
     def save(self, commit=True):
         user_profile = super().save(commit=False)
-        if self.has_changed() and 'email' in self.changed_data:
-            user_profile.user.email = self.cleaned_data['email']
+        if self.has_changed():
+            if 'email' in self.changed_data:
+                user_profile.user.email = self.cleaned_data['email']
+            if 'username' in self.changed_data:
+                user_profile.user.username = self.cleaned_data['username']
             user_profile.user.save()
         if commit:
             user_profile.save()

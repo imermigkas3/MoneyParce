da803ac81bc26196d8df1da3015de1b2b044f935
imermigkas3 <mermigasj@gmail.com>
2025-04-26 21:50:20 -0400

    no empty budgets

--- budgets/views.py
+++ budgets/views.py
@@ -23,7 +23,7 @@ def index(request):
 @login_required
 def create_budget(request):
     form_error = None
-    if request.method == "POST" and request.POST['title'] != "":
+    if request.method == "POST":
         b = Budget()
         b.user = request.user
         b.title = request.POST.get("title","").strip()
@@ -36,18 +36,19 @@ def create_budget(request):
         try:
             b.amount = Decimal(amt)
         except (InvalidOperation, ValueError):
+            form_error = "You need to have a title and an amount"
             b.amount = Decimal("0")
+        if b.title != "" and b.amount > 0:
+            exists = Budget.objects.filter(
+                user=request.user,
+                category=b.category,
+            ).exists()
 
-        exists = Budget.objects.filter(
-            user=request.user,
-            category=b.category,
-        ).exists()
-
-        if exists:
-            form_error = f"You already have a budget for this category: {b.category}"
-        else:
-            b.save()
-            return redirect("Budgets.index")
+            if exists:
+                form_error = f"You already have a budget for this category: {b.category}"
+            else:
+                b.save()
+                return redirect("Budgets.index")
 
     # if GET or missing title, just show the form again
     return render(request,
@@ -58,7 +59,7 @@ def create_budget(request):
 def edit_budget(request, id):
     budget = get_object_or_404(Budget, id=id, user=request.user)
     form_error = None
-    if request.method == "POST" and request.POST['title'] != "":
+    if request.method == "POST" and request.POST['title'] != "" and request.POST['amount'] != "0":
         budget.title = request.POST.get("title","").strip()
         budget.description = request.POST.get("description","").strip()
         budget.category = request.POST.get("category","").strip()
@@ -73,6 +74,9 @@ def edit_budget(request, id):
             form_error = "Please enter a valid number."
         except ValidationError as e:
             form_error = e.message_dict.get('amount', ["Invalid amount"])[0]
+
+    elif request.method == "POST":
+        form_error = "You need to have a title and an amount"
 
 
     # on GET, just show the form with the existing values
